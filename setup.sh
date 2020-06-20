13c1c24d4c57584b58e3775043bb6d6d9f1879eb
wollllll <sakin50guramu@gmail.com>
2020-06-20 15:04:10 +0900

    box変更

--- setup.sh
+++ setup.sh
@@ -1,19 +1,18 @@
+sudo yum -y update
+
 # install docker
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker vagrant
 
 # install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # install git 
-sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-sudo yum -y install git2u yum-utils
+sudo yum -y install git
 
 # install vim
 sudo yum -y install vim
